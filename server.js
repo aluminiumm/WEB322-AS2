9ad02a65b12ba9c8807b2a495852341a407399b1
Ayumi Ueda <ayumi.reg@gmail.com>
2021-03-14 21:16:15 -0400

    Modified for HEROKU

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ app.post("/formRegistration",(req,res)=>{
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT , ()=>
 {
         console.log(`Web application is up and running!!!`);
